aa13883040ef67afae47ca0862c668d528ccec5d
vivekannan <vivek7641@gmail.com>
2016-07-25 15:05:41 +0530

    Reworked media controls panel.

--- src/js/features/playpause.js
+++ src/js/features/playpause.js
@@ -7,7 +7,7 @@
     MediaElementPlayer.prototype.buildplaypause = function() {
         var t = this;
         
-        $('<div class="mejs-button mejs-playpause-button mejs-play" >' +
+        $('<div class="mejs-button mejs-playpause-button msjs-play" >' +
             '<button type="button" title="' + t.options.playpauseText + '" aria-label="' + t.options.playpauseText + '"></button>' +
         '</div>')
         .appendTo(t.controls)
